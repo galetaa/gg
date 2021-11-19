b1d08fcea29ac8a7c906e6dcd9473c3a6ef0a243
galetka <mihailiy05@icloud.com>
2021-11-19 18:00:44 +0300

    circles v1

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget
 from PIL.ImageQt import ImageQt
 from PyQt5.QtGui import QPixmap
 from PIL import Image, ImageDraw
 from random import randint
-import sys
+from PyQt5 import QtCore
+import sys, PyQt5
 
 
-class MyWidget(QWidget):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(400, 300)
+        self.horizontalLayout = PyQt5.QtWidgets.QHBoxLayout(Form)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.verticalLayout = PyQt5.QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.label = PyQt5.QtWidgets.QLabel(Form)
+        self.label.setObjectName(u"label")
+
+        self.verticalLayout.addWidget(self.label)
+
+        self.pushButton = PyQt5.QtWidgets.QPushButton(Form)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.verticalLayout.addWidget(self.pushButton)
+
+        self.horizontalLayout.addLayout(self.verticalLayout)
+
+        self.retranslateUi(Form)
+
+        PyQt5.QtCore.QMetaObject.connectSlotsByName(Form)
+
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(PyQt5.QtCore.QCoreApplication.translate("Form", u"Form", None))
+        self.label.setText("")
+        self.pushButton.setText(PyQt5.QtCore.QCoreApplication.translate("Form", u"run", None))
+    # retranslateUi
+
+
+class MyWidget(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.label.hide()
         self.pushButton.clicked.connect(self.run)
 
@@ -22,7 +56,7 @@ class MyWidget(QWidget):
             d = randint(10, 300)
             x = randint(0, 500)
             y = randint(0, 500)
-            draw.ellipse(((x, y), (x + d, y + d)), (255, 211, 0))
+            draw.ellipse(((x, y), (x + d, y + d)), (randint(1, 255), randint(1, 255), randint(1, 255)))
         self.imqt = ImageQt(self.im)
         self.pixmap = QPixmap.fromImage(self.imqt)
         self.label.setPixmap(self.pixmap)
